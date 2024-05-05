d1ba1473a151e4598277b29f6b42c62de85b6ce8
Santiago Millan <santiagomillanpardo@gmail.com>
2024-05-05 01:07:20 -0500

    Feature: terminar componentes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@
 //   console.log(revenue);
 //   return <p>Dashboard Page</p>;
 // }
-import { fetchLatestInvoices, fetchRevenue } from '../lib/data';
+import { fetchLatestInvoices, fetchRevenue, fetchCardData } from '../lib/data';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
@@ -13,23 +13,27 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
 
-//TODO terminar componentes
-
 export default async function Page() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
